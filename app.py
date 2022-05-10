01f98a33339e87e4da9cf5bc5e9cde60f90ffb70
abhinav80066 <abhinav.chaudhary800@gmail.com>
2022-05-10 18:10:03 +0530

    Completed web app using streamlit

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import pandas_datareader as data
 from keras.models import load_model
+from sklearn.manifold import spectral_embedding
 import streamlit as st
 
 start = '2010-01-01'
@@ -38,3 +39,50 @@ plt.plot(ma100)
 plt.plot(ma200)
 plt.plot(df.Close)
 st.pyplot(fig)
+
+#splitting data into training and testing
+
+data_training = pd.DataFrame(df['Close'][0:int(len(df)*0.70)])
+data_testing = pd.DataFrame(df['Close'][int(len(df)*0.70):int(len(df))])
+
+from sklearn.preprocessing import MinMaxScaler
+scaler = MinMaxScaler(feature_range=(0,1))
+
+data_training_array = scaler.fit_transform(data_training)
+
+
+
+# Load my model
+model = load_model('keras_model.h5')
+
+#Testing Part
+past_100_days = data_training.tail(100)
+final_df = past_100_days.append(data_testing, ignore_index = True)
+input_data = scaler.fit_transform(final_df)
+
+x_test = []
+y_test = []
+
+for i in range(100, input_data.shape[0]):
+    x_test.append(input_data[i-100:i])
+    y_test.append(input_data[i,0])
+
+
+x_test, y_test = np.array(x_test), np.array(y_test)
+Y_predicted = model.predict(x_test)
+scaler = scaler.scale_ 
+
+scale_factor = 1/scaler[0]
+Y_predicted = Y_predicted * scale_factor
+y_test = y_test * scale_factor
+
+# Final Graph
+st.subheader('Predictions vs Original')
+fig2 = plt.figure(figsize=(12,6))
+plt.plot(y_test, 'b', label = 'Original Price')
+plt.plot(Y_predicted, 'r', label = 'Predicted Price')
+plt.xlabel('Time')
+plt.ylabel('Price')
+plt.legend() 
+st.pyplot(fig2)
+
